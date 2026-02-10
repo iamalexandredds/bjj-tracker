a0867d7feb69d940e38a1f60addf90a5b020c3fa
iamalexandredds <i.am.alexandredds@gmail.com>
2026-02-10 19:03:45 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+// Queste sono le tue chiavi specifiche
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://zwwmzmoliiharftzodse.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3d216bW9saWloYXJmdHpvZHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MzE3NDAsImV4cCI6MjA4NjMwNzc0MH0.xuvQaWUOGa25mESKXDonwneG2Tkd2f576F6lx9q4mRs";
 
-export const supabase = (supabaseUrl.startsWith('https')) 
-  ? createClient(supabaseUrl, supabaseAnonKey) 
-  : null as any;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
