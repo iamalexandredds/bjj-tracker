40825753247cd8201a764d5fb75cc4858a25dd3d
iamalexandredds <i.am.alexandredds@gmail.com>
2026-02-11 16:49:48 +0100

    Update AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,7 +1,4 @@
-"use client"; 
-
 import { ReactNode } from 'react';
-// FIX: Assicurati che AppSidebar usi export function AppSidebar...
 import { AppSidebar } from './AppSidebar';
 
 interface AppLayoutProps {
@@ -11,12 +8,9 @@ interface AppLayoutProps {
 export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="min-h-screen bg-background">
-      {/* Sidebar fissa a sinistra */}
       <AppSidebar />
-      
-      {/* Contenuto principale */}
       <main className="pl-64 min-h-screen transition-all duration-300">
-        <div className="container py-6 px-8 mx-auto">
+        <div className="container py-6 px-8">
           {children}
         </div>
       </main>
